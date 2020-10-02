0d52ffeb79164d2dc47d0d9ae4144047c3341610
Efrain Aperador <efrain.aperador@gmail.com>
2020-10-01 23:43:34 -0500

    agregar pantalla de login y utilidades

--- public/src/app/login/login.component.ts
+++ public/src/app/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+  visible: String = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleModal() {
+    this.visible = this.visible === "" ? "visible" : "";
   }
 
 }
