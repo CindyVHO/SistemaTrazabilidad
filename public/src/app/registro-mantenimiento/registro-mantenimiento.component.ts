dce63c88602dbe7c983696d92ba07991a8b08b99
Efrain Aperador <efrain.aperador@gmail.com>
2020-10-08 23:26:06 -0500

    update forms date pickers

--- public/src/app/registro-mantenimiento/registro-mantenimiento.component.ts
+++ public/src/app/registro-mantenimiento/registro-mantenimiento.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registro-mantenimiento',
@@ -8,9 +8,37 @@ import {FormControl} from '@angular/forms';
   encapsulation: ViewEncapsulation.None
 })
 export class RegistroMantenimientoComponent implements OnInit {
+  public form: FormGroup = new FormGroup({
+    calculatedTime: new FormControl(''),
+  });
+
+  duracionTotal = "00:00";
+  horaInicial;
+  horaFinal;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateHoraTotal() {
+    if(this.horaInicial && this.horaFinal) {
+      let horaI = parseInt(this.horaInicial.split(":")[0]),
+      minutoI = parseInt(this.horaInicial.split(":")[1]),
+      horaF = parseInt(this.horaFinal.split(":")[0]),
+      minutoF = parseInt(this.horaFinal.split(":")[1]);
+
+      if(minutoF < minutoI) {
+        minutoF += 60;
+        horaF = horaF >= 1 ? horaF-- : 23;
+      }
+
+      const minutoTotal = (minutoF - minutoI)%60,
+      horaTotal = Math.abs((horaF - horaI) + ((minutoF - minutoI)/60 > 1 ? (minutoF - minutoI)/60 : 0));
+      
+      this.duracionTotal = (horaTotal < 10 ? "0" + horaTotal: horaTotal) + ":" + 
+                              (minutoTotal < 10 ? "0" + minutoTotal : minutoTotal);
+    }
   }
 
   dateChanged(fecha, event) {
@@ -18,11 +46,13 @@ export class RegistroMantenimientoComponent implements OnInit {
   }
 
   initHourChanged(horaInicio, event) {
-    console.log("INIT HOUR CHANGED", horaInicio.value);
+    this.horaInicial = horaInicio.value;
+    this.updateHoraTotal();
   }
 
   endHourChanged(horaFin, event) {
-    console.log("END HOUR CHANGED", horaFin.value);
+    this.horaFinal = horaFin.value;
+    this.updateHoraTotal();
   }
 
 }
