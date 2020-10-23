64c7d9bb95bf7dc27d6fd44d3ca1fa8608c5fba6
Cindy Hernaández <cvho31@gmail.com>
2020-10-22 22:02:27 -0500

    :computer: More tables

---
+++ model/hoja-vida-sql.js
@@ -0,0 +1,80 @@
+const connection = require('./client');
+const uuid = require('uuid');
+
+const hojaVida = (() => {
+    const createTable = 'CREATE TABLE IF NOT EXISTS hoja_vida (' +
+        'idhojavida uuid DEFAULT PRIMARY KEY, ' +
+        'tipo_riesgo VARCHAR NOT NULL, ' +
+        'tipo_funcion VARCHAR NOT NULL, ' +
+        'voltaje_maximo DECIMAL NOT NULL, ' +
+        'voltaje_minimo DECIMAL NOT NULL, ' +
+        'corriente_maxima DECIMAL NOT NULL, ' +
+        'corriente_minima DECIMAL NOT NULL, ' +
+        'potencia DECIMAL NOT NULL, ' +
+        'frecuencia DECIMAL NOT NULL, ' +
+        'temperatura DECIMAL NOT NULL, ' +
+        'velocidad DECIMAL NOT NULL, ' +
+        'capacidad DECIMAL NOT NULL, ' +
+        'presion DECIMAL NOT NULL, ' +
+        'alimentacion VARCHAR NOT NULL, ' +
+        'adquisicion VARCHAR NOT NULL, ' +
+        'uso VARCHAR NOT NULL, ' +
+        'FOREIGN KEY (equipo) REFERENCES equipo (idequipo)' +
+        ');';
+
+    function validateExists() {
+        return new Promise((resolve, reject) => {
+            connection.sqlQuery(createTable).then(() => {
+                resolve();
+            }).catch((err) => {
+                reject();
+            });
+        });
+    }
+
+    function insertHojaVida(hojaVida) {
+        return new Promise((resolve, reject) => {
+            validateExists().then(() => {
+                let sqlQuery = 'INSERT INTO hoja_vida VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) RETURNING idhojavida';
+                let query = {
+                    text: sqlQuery,
+                    values: [
+                        uuid.v1(),
+                        hojaVida.tipo_riesgo,
+                        hojaVida.tipo_funcion,
+                        hojaVida.voltaje_maximo,
+                        hojaVida.voltaje_minimo,
+                        hojaVida.corriente_maxima,
+                        hojaVida.corriente_minima,
+                        hojaVida.potencia,
+                        hojaVida.frecuencia,
+                        hojaVida.temperatura,
+                        hojaVida.velocidad,
+                        hojaVida.capacidad,
+                        hojaVida.presion,
+                        hojaVida.alimentacion,
+                        hojaVida.adquisicion,
+                        hojaVida.uso,
+                        hojaVida.equipo
+                    ]
+                };
+
+                connection.sqlQuery(query).then((res) => {
+                    resolve(res);
+                }).catch((err) => {
+                    reject(err);
+                });
+            }).catch((err)=>{
+                reject(err);
+            });
+
+        });
+    }
+
+    return {
+        insertHojaVida: insertHojaVida
+    }
+
+})();
+
+module.exports = hojaVida;
---
+++ model/mantenimiento-sql.js
@@ -0,0 +1,72 @@
+const connection = require('./client');
+const uuid = require('uuid');
+
+const mantenimiento = (() => {
+    const createTable = 'CREATE TABLE IF NOT EXISTS mantenimiento (' +
+        'idmantenimiento uuid DEFAULT PRIMARY KEY, ' +
+        'ubicacion_equipo VARCHAR NOT NULL, ' +
+        'tipo_mantenimiento VARCHAR NOT NULL, ' +
+        'material VARCHAR NOT NULL, ' +
+        'diagnostico VARCHAR NOT NULL, ' +
+        'trabajo_ejecutado VARCHAR NOT NULL, ' +
+        'observacion_final VARCHAR NOT NULL, ' +
+        'fecha TIMESTAMP NOT NULL, ' +
+        'hora_inicio TIMESTAMP NOT NULL, ' +
+        'hora_fin TIMESTAMP NOT NULL, ' +
+        'firma VARCHAR NOT NULL, ' +
+        'FOREIGN KEY (user) REFERENCES user (iduser)' +
+        'FOREIGN KEY (equipo) REFERENCES equipo (idequipo)' +
+        ');';
+
+    function validateExists() {
+        return new Promise((resolve, reject) => {
+            connection.sqlQuery(createTable).then(() => {
+                resolve();
+            }).catch((err) => {
+                reject();
+            });
+        });
+    }
+
+    function insertMantenimiento(mantenimiento) {
+        return new Promise((resolve, reject) => {
+            validateExists().then(() => {
+                let sqlQuery = 'INSERT INTO mantenimientos VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) RETURNING idmantenimiento';
+                let query = {
+                    text: sqlQuery,
+                    values: [
+                        uuid.v1(),
+                        mantenimiento.ubicacion_equipo,
+                        mantenimiento.tipo_mantenimiento,
+                        mantenimiento.material,
+                        mantenimiento.diagnostico,
+                        mantenimiento.trabajo_ejecutado,
+                        mantenimiento.observacion_final,
+                        mantenimiento.fecha,
+                        mantenimiento.hora_inicio,
+                        mantenimiento.hora_fin,
+                        mantenimiento.firma,
+                        mantenimiento.user,
+                        mantenimiento.equipo
+                    ]
+                };
+
+                connection.sqlQuery(query).then((res) => {
+                    resolve(res);
+                }).catch((err) => {
+                    reject(err);
+                });
+            }).catch((err)=>{
+                reject(err);
+            });
+
+        });
+    }
+
+    return {
+        insertMantenimiento: insertMantenimiento
+    }
+
+})();
+
+module.exports = mantenimiento;
