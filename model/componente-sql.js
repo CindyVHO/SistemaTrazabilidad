cb6abe426c803bedf736385e8b9ce177541e6555
Cindy Hernaández <cvho31@gmail.com>
2020-10-22 21:14:42 -0500

    :computer: Component table

---
+++ model/componente-sql.js
@@ -0,0 +1,56 @@
+const connection = require('./client');
+const uuid = require('uuid');
+
+const componente = (() => {
+    const createTable = 'CREATE TABLE IF NOT EXISTS componente (' +
+        'idcomponente uuid DEFAULT PRIMARY KEY, ' +
+        'componente VARCHAR NOT NULL, ' +
+        'funcionamiento BOOLEAN NOT NULL, ' +        
+        'observaciones VARCHAR NOT NULL, ' +
+        'FOREIGN KEY (rutina) REFERENCES rutina (idrutina)' +
+        ');';
+
+    function validateExists() {
+        return new Promise((resolve, reject) => {
+            connection.sqlQuery(createTable).then(() => {
+                resolve();
+            }).catch((err) => {
+                reject();
+            });
+        });
+    }
+
+    function insertComponente(componente) {
+        return new Promise((resolve, reject) => {
+            validateExists().then(() => {
+                let sqlQuery = 'INSERT INTO componente VALUES ($1,$2,$3,$4,$5) RETURNING idcomponente';
+                let query = {
+                    text: sqlQuery,
+                    values: [
+                        uuid.v1(),
+                        componente.componente,
+                        componente.funcionamiento,
+                        componente.observaciones,
+                        componente.rutina
+                    ]
+                };
+
+                connection.sqlQuery(query).then((res) => {
+                    resolve(res);
+                }).catch((err) => {
+                    reject(err);
+                });
+            }).catch((err)=>{
+                reject(err);
+            });
+
+        });
+    }
+
+    return {
+        insertComponente: insertComponente
+    }
+
+})();
+
+module.exports = componente;
